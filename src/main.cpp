d41c206279890cae6d3a7b99fe3aea0333496fce
Phan Son Loc <phansonloc1999@gmail.com>
2021-08-05 14:37:25 +0700

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -8,38 +8,43 @@ int main(int argc, char const *argv[])
 {
 	if (argc == 1)
 	{
-
-		cout << "mycat: missing operands/operators" << endl;
-		cout << "Try 'mycat -h' for more information." << endl;
+		string line;
+		while (true)
+		{
+			getline(cin, line, '\n');
+			cout << line << endl;
+		}
 		return 0;
 	}
 
-	if (strcmp(argv[1], "-h") == 0)
+	if (argc == 2 && strcmp(argv[1], "-h") == 0)
 	{
-		cout << "Usage: mycat [TARGET FILE PATH]" << endl;
+		cout << "Usage: mycat [IN FILE PATH]" << endl;
 		cout << "Usage: mycat > [OUT FILE PATH]" << endl;
-		cout << "Usage: mycat [IN FILE PATH] > [OUT FILE PATH]" << endl;
+		cout << "Usage: mycat > [OUT FILE PATH]" << endl;
+		cout << "Usage: mycat [IN FILE PATH] [OUT FILE PATH]" << endl;
 		return 0;
 	}
 
 	ifstream inStream(argv[1], ios::in);
-	ofstream outStream(argv[3], ios::out | ios::trunc);
-	if (inStream)
+	ofstream outStream(argv[2], ios::out | ios::trunc);
+	string line;
+	while (inStream.is_open() && !inStream.eof())
 	{
-		string line;
-		while (!inStream.eof())
-		{
-			getline(inStream, line, '\n');
-			cout << line << endl;
+		getline(inStream, line, '\n');
+		cout << line << endl;
 
-			if (outStream)
-			{
-				outStream << line << endl;
-			}
+		if (outStream)
+		{
+			outStream << line << endl;
 		}
 	}
-	else
-		cout << "ERROR File doesn't exist" << endl;
+	
+	if (!inStream)
+	{
+		string inFilePath = argv[1];
+		cout << inFilePath + ": No such file" << endl;
+	}
 
 	return 0;
 }
